19b27cdd55a1ad3eb2f1ead57e79d6b477336198
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2017-07-05 06:04:24 -0500

    Details Layout

--- app/src/main/java/io/github/gianpamx/superheros/DetailsActivity.java
+++ app/src/main/java/io/github/gianpamx/superheros/DetailsActivity.java
@@ -1,7 +1,7 @@
 package io.github.gianpamx.superheros;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class DetailsActivity extends AppCompatActivity {
 
--- app/src/main/java/io/github/gianpamx/superheros/DetailsFragment.java
+++ app/src/main/java/io/github/gianpamx/superheros/DetailsFragment.java
@@ -6,11 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsFragment extends Fragment {
+    private ImageView thumbnail;
+    private TextView nameTextView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.details_fragment, container, false);
+        View view = inflater.inflate(R.layout.details_fragment, container, false);
+
+        thumbnail = view.findViewById(R.id.thumbnail);
+        nameTextView = view.findViewById(R.id.name);
+
+        return view;
     }
 }
